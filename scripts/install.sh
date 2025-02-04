36d40dbc99271b2d085a92898c8632a9900127b9
lizhieffe <lizhieffe@gmail.com>
2025-02-03 22:38:19 -0800

    Fix the brokage of running project/run_sentiment.py

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,8 @@ pushd
 
 cd $(git rev-parse --show-toplevel)
 
-!python -m pip install -r requirements.txt
-!python -m pip install -r requirements.extra.txt
-!python -m pip install -Ue .
+python -m pip install -r requirements.txt
+python -m pip install -r requirements.extra.txt
+python -m pip install -Ue .
 
 popd
--- scripts/run_fast_tensor_cpu.sh
+++ scripts/run_fast_tensor_cpu.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+pushd
+
+cd $(git rev-parse --show-toplevel)
 python run_fast_tensor.py --BACKEND cpu --HIDDEN 100 --DATASET split --RATE 0.05
 
+popd
